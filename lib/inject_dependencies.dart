756860b0c445579bae2597ca919b1a6cf7ec8fbd
adarista012 <ad.arista012@gmail.com>
2024-10-17 15:17:19 -0400

    feature: inject dependencies

--- lib/inject_dependencies.dart
+++ lib/inject_dependencies.dart
@@ -1,1 +1,27 @@
+import 'package:get/get.dart';
+import 'package:outcode/app/app_constants.dart';
+import 'package:outcode/app/app_controller.dart';
+import 'package:outcode/app/data/implementations/weather_api_repository_implementation.dart';
+import 'package:outcode/app/data/source/remote/weather_api.dart';
+import 'package:outcode/app/domain/repositories/weather_api_repository.dart';
+import 'package:outcode/app/presentation/pages/home/home_controller.dart';
 
+Future<void> injectDependencies() async {
+  final appControllerRepository = Get.put(AppController());
+
+  final weatherApi = Get.put(WeatherApi(
+    baseURL: AppConstants.BASE_URL,
+    apiKey: AppConstants.API_KEY,
+  ));
+
+  Get.lazyPut<WeatherApiRepository>(
+      () => WeatherApiRepositoryImplementation(weatherApi: weatherApi));
+
+  final weatherApiRepository = Get.find<WeatherApiRepository>();
+
+  Get.lazyPut(
+    () => HomeController(
+        appControllerRepository: appControllerRepository,
+        weatherApiRepository: weatherApiRepository),
+  );
+}
